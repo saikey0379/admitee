8463492824b47357b375ce51985b16e1bd6e254c
saikey0379 <316851126@qq.com>
2022-10-27 12:04:58 +0800

    labelforce admiteed-smooth-force

--- pkg/api/v1alpha1/const.go
+++ pkg/api/v1alpha1/const.go
@@ -2,8 +2,8 @@ package v1alpha1
 
 const (
 	// set like crd
-	Group    = "validating.example.com"
-	Version  = "v1alpha1"
-	Resource = "smooths"
-	Label    = "admiteed-validate"
+	Group      = "validating.example.com"
+	Version    = "v1alpha1"
+	Resource   = "smooths"
+	LabelForce = "admiteed-smooth-force"
 )
